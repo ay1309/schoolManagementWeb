bd1f1ff9a5aaa6a0128ac3b03da839754521281a
ay1309 <ann_yoto@naver.com>
2024-05-16 23:21:21 -0600

    commits

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import Handlebars from 'handlebars'; // importar Handlebars
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = process.env.PORT || 4000;
+
+Handlebars.registerHelper('section', function(name, options) {
+    if (!this._sections) {
+        this._sections = {};
+    }
+    this._sections[name] = options.fn(this);
+    return null;
+});
+
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+}));
+
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// configuración de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
